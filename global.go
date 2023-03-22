eb84a912ed5ac21ea7b042d14d3c11d475170382
krishpranav <krisna.pranav@gmail.com>
2023-03-22 12:12:06 +0530

    global: Singleton + Transient + NamedResolve

--- global.go
+++ global.go
@@ -10,12 +10,40 @@ func SingletonLazy(resolver interface{}) error {
 	return Global.SingletonLazy(resolver)
 }
 
+func NamedSingleton(name string, resolver interface{}) error {
+	return Global.NamedSingleton(name, resolver)
+}
+
+func NamedSingletonLazy(name string, resolver interface{}) error {
+	return Global.NamedSingletonLazy(name, resolver)
+}
+
+func Transient(resolver interface{}) error {
+	return Global.Transient(resolver)
+}
+
+func TransientLazy(resolver interface{}) error {
+	return Global.TransientLazy(resolver)
+}
+
+func NamedTransient(name string, resolver interface{}) error {
+	return Global.NamedTransient(name, resolver)
+}
+
+func NamedTransientLazy(name string, resolver interface{}) error {
+	return Global.NamedTransientLazy(name, resolver)
+}
+
 func Reset() {
 	Global.Reset()
 }
 
 func Call(receiver interface{}) error {
 	return Global.Call(receiver)
+}
+
+func Resolve(abstraction interface{}) error {
+	return Global.Resolve(abstraction)
 }
 
 func NamedResolve(abstraction interface{}, name string) error {
